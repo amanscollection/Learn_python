178251c49a671c25c621ac35d3c940839026ad4c
Amandeep <amanscollection@gmail.com>
2025-05-11 01:57:34 +0530

    Added Escape.py

--- Escape.py
+++ Escape.py
@@ -1,2 +1,16 @@
+# \"
+# \'
+# \\
+# \n
+
 course = "Python \"Programming"
+print(course)
+
+course = "Python \'Programming"
+print(course)
+
+course = "Python \\Programming"
+print(course)
+
+course = "Python \nProgramming"
 print(course)
