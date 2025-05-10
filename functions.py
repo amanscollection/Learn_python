9f32bc51f225be578a0ed7ca83c5808e3da065b7
Amandeep <amanscollection@gmail.com>
2025-05-11 01:29:04 +0530

    update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,15 @@
-course = "Pythom programming"
+course = "Python programming"
 print(len(course))
 print(course[0])
+
+
+print(course[-1])
+
+
+print(course[0:3])
+
+print(course[0:])
+
+print(course[:3])
+
+print(course[:])
