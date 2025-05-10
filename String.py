4c1c51aab15c7809e851ed1c010f8735bfacb19d
Amandeep <amanscollection@gmail.com>
2025-05-11 02:20:59 +0530

    Added Strings

---
+++ String.py
@@ -0,0 +1,21 @@
+course = "Python Programming"
+
+# upper method
+print(course.upper())
+
+# lower method
+print(course.lower())
+
+# make first letter of captize
+print(course.title())
+
+
+input = "  Python Programming"
+print(input.strip())
+
+print(course.find("Pro"))
+
+print(course.replace("P", "J"))
+
+print("Pro" in course)
+print("swift" not in course)
---
+++ formatted_strings.py
@@ -0,0 +1,10 @@
+first = "Amandeep"
+last = "Singh"
+
+# Old method of concaction
+full = first + " " + last
+print(full)
+
+# new method of concaction
+full = f"{first} {last}"
+print(full)
