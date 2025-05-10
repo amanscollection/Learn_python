0d3588a9db9ae7989f7156564826e8024ae113c7
Amandeep <amanscollection@gmail.com>
2025-05-11 01:36:26 +0530

    Added

--- functions.py
+++ functions.py
@@ -1,15 +1,20 @@
 course = "Python programming"
 print(len(course))
+
+# print of P
 print(course[0])
 
-
+# whole string
 print(course[-1])
 
-
+# print of pyt
 print(course[0:3])
 
+# whole string
 print(course[0:])
 
+# print of pyt
 print(course[:3])
 
+# whole string
 print(course[:])
--- hello_world.py
+++ hello_world.py
@@ -3,8 +3,8 @@ print("Hello World")
 print("switch off")
 print("Switch on")
 
+# 10 digit of star
 print("*" * 10)
 
+# print
 print("Welcome to the Python")
-
-
